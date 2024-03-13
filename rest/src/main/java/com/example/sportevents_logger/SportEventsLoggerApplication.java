578e4376156749c5e84f800a02e7147e8eca4fe8
Emo0o0 <emil.kolev1102@gmail.com>
2024-03-13 18:52:03 +0200

    Logger Service

---
+++ rest/src/main/java/com/example/sportevents_logger/SportEventsLoggerApplication.java
@@ -0,0 +1,11 @@
+package com.example.sportevents_logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportEventsLoggerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SportEventsLoggerApplication.class, args);
+    }
+}
