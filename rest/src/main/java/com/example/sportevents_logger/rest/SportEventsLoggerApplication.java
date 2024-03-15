c61c49a2cda446aea6052e1cf6e4079a0d574cf9
Emo0o0 <emil.kolev1102@gmail.com>
2024-03-15 13:30:40 +0200

    package change

--- rest/src/main/java/com/example/sportevents_logger/rest/SportEventsLoggerApplication.java
+++ rest/src/main/java/com/example/sportevents_logger/rest/SportEventsLoggerApplication.java
@@ -1,4 +1,4 @@
-package com.example.sportevents_logger;
+package com.example.sportevents_logger.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
